466b315b112add086563fd15c0caeebd00792e15
yanxt <wsxiaotiana@163.com>
2021-05-12 15:33:15 +0800

    完善前缀和逻辑

--- src/class02/Code01_PreSum.java
+++ src/class02/Code01_PreSum.java
@@ -1,10 +1,38 @@
 package class02;
 
 /**
- * @description: 预处理求和
+ * @description: 前缀和
  * @author: yanxiaotian
  * @create: 2021-05-07 16:37
  **/
 public class Code01_PreSum {
 
+    public static class RangeSum1{
+        private int[] arr;
+        public RangeSum1(int[] array){
+            arr=array;
+        }
+        public int rangeSum(int L,int R){
+            int sum=0;
+            for(int i=L;i<R;i++){
+                sum+=arr[i];
+            }
+            return sum;
+        }
+    }
+
+    public static class RangeSum2{
+        private int[] preSum;
+
+        public RangeSum2(int[] array){
+            preSum[0]=array[0];
+            for(int i=1;i<array.length;i++){
+                preSum[i]=preSum[i-1]+array[i];
+            }
+        }
+
+        public int ranSum(int L,int R){
+            return L==0? preSum[R]:preSum[R]-preSum[L-1];
+        }
+    }
 }
